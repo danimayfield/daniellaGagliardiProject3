1c3201317204e56761f80bcef0839383f0153b0f
Dani <daniella.m.gagliardi@gmail.com>
2021-11-23 18:47:24 -0500

    added cartcount functionality & styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './index.css';
 import { useState, useEffect } from 'react';
 import firebase from './Firebase.js';
-// import {MouseOver, MouseOut} from 'Hover.js';
+import Header from './Header';
+
 
 function App() {
   // Piece of state to hold data array:
   const [listingInfo, setListingInfo] = useState([])
+  // Piece of state to hold cart items in an array:
+  const [cart, setCart] = useState([])
+  // Piece of state holding the count of items in the cart:
+  const [cartCount, setCartCount] = useState(0)
 
   // Use effect to hold database info:
   useEffect(() => {
@@ -17,44 +22,66 @@ function App() {
 
       // variable to hold data object from firebase
       const data = response.val();
-      console.log("DATA -->",data)
-      // iterate through the data object
-      // for (let property in data.results) {
-      //   console.log("PROPERTY",property);
-      //   // Use spread operator to add each data object into the state array of listingInfo
-      //   setListingInfo(...listingInfo, data.results[property])
-      //   console.log("LISTINGINFO",listingInfo)
-      // }
+
+      // Set the data to the state
       setListingInfo(data.results)
     })
 
   }, [])
 
-  useEffect(() => console.log('info->', listingInfo), [listingInfo])
+
+  function addToCart(result) {
+    setCart([...cart, result])
+  }
+
+  useEffect(() => {
+    setCartCount(cart.length)
+  }, [cart])
+
+
+
+  useEffect(() => console.log('cart info->', cart), [cart])
   //------------
   return (
-    <main>
-      <h1>Sprinkling Rainbows Shop</h1>
-      <h2>Filter Area</h2>
-      <div className="inventoryGrid">
-        {
-          // Map through listingInfo & use the results to populate info!
-          listingInfo.map(results => {
-            return (
-              <div key={results.listing_id} className="inventoryCard">
-                <img src={results.image} alt={results.title} className="inventoryImage" />
-                <h3>{results.title}</h3>
-                <p>$ {(results.price.amount) / (results.price.divisor)}</p>
-              </div>
-            )
-          })
-        }
-      </div>
+    <div>
+      <i class="fas fa-shopping-cart cart"><span className="cartCount">{cartCount}</span></i>
+      
+      <Header />
+      <main>
+        <h1>Sprinkling Rainbows Shop</h1>
+        <h2>Filter Area</h2>
+        <div className="inventoryGrid">
+          {
+            // Map through listingInfo & use the results to populate info!
+            listingInfo.map(result => {
+              return (
+                <div key={result.listing_id} className="inventoryCard">
+                  <div className="inventoryCart" onClick={() => addToCart(result)}>
+                    <i className="fas fa-cart-plus addCartIcon"></i>
+                  </div>
+                  <img src={result.image} alt={result.title} className="inventoryImage" />
+                  <h3>{result.title}</h3>
+                  <p>$ {(result.price.amount) / (result.price.divisor)}</p>
 
-    </main>
+                </div>
+              )
+            })
+          }
+        </div>
+
+      </main>
+    </div>
   );
 }
 
 
 export default App;
+
+  // function MouseOver(event) {
+  //   event.classList.remove("none")
+  // }
+
+  // function MouseOut(event) {
+  //   event.classList.add("none")
+  // }
 
